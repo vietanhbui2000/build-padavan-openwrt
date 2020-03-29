1b0781209a6a9984a395246b2b8fc973ba9bad3d
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-29 13:45:45 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -2,3 +2,10 @@
 
 # Add themes and theme changer to Padavan firmware
 cp scripts/padavan/www/n56u_ribbon_fixed /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+
+# Remove selected themes
+rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
+rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
+rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
+rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
+rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
