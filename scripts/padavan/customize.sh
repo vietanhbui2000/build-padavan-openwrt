6fc4b65ddc4a08fa6f2c42c2d5dceeca79f525bd
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-29 21:52:27 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -5,4 +5,4 @@
 
 sudo apt-get install unzip
 wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14.zip
-unzip PadavanThemePack_v2.4.14.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+unzip -o PadavanThemePack_v2.4.14.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
