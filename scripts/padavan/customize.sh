1f3ca0f43e8999d5d4e1a9c421af6405f816d26c
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-30 00:47:14 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -9,16 +9,16 @@
 
 # Download file(s)
 # wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14.zip
-# wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14_Lite.zip
+wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14_Lite.zip
 
 # Unzip file(s)
 # unzip -o PadavanThemePack_v2.4.14.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
-# unzip -o PadavanThemePack_v2.4.14_Lite.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+unzip -o PadavanThemePack_v2.4.14_Lite.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
 
 # Delete file(s)
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
 # rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/white-theme
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/white-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
