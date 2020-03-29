03fe47ddc10a6d2fc27b04d7d860846a03d935e7
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-30 00:21:50 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -3,19 +3,22 @@
 # Modifications made by: Việt Anh Bùi | https://github.com/vietanhbui2000 #
 ###########################################################################
 
+# Add themes and theme changer to Padavan firmware
 # Install necessary package(s)
 # sudo apt-get install unzip
 
 # Download file(s)
-wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14.zip
+# wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14.zip
+wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/blob/master/scripts/padavan/PadavanThemePack_v2.4.14_lite.zip
 
 # Unzip file(s)
-unzip -o PadavanThemePack_v2.4.14.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+# unzip -o PadavanThemePack_v2.4.14.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+unzip -o PadavanThemePack_v2.4.14_lite.zip -d /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
 
 # Delete file(s)
 rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
-rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
+# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
 rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
 rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
-# rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/white-theme
+rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/white-theme
 rm -r /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
