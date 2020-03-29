7df2a052a4b998994b5378146bcf09161f8d3696
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-29 13:56:46 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Add themes and theme changer to Padavan firmware
-cp scripts/padavan/www/n56u_ribbon_fixed /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
+cp -R /scripts/padavan/www/n56u_ribbon_fixed /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
 
 # Remove selected themes
-rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
-rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
-rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
-rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
-rm /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
+rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
+rm -R  /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
+rm -R  /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
+rm -R  /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
+rm -R  /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
