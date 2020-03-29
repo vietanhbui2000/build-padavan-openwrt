14cfd3d5253bbd9ddce5377b9ae99eeee2fae20c
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-03-29 14:36:39 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -4,8 +4,8 @@
 cp -R scripts/padavan/www/n56u_ribbon_fixed /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed
 
 # Remove selected themes
-rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
-rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
-rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
-rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
-rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
+# rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue2-theme
+# rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/blue-theme
+# rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey2-theme
+# rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/grey-theme
+# rm -R /opt/rt-n56u/trunk/user/www/n56u_ribbon_fixed/yellow-theme
