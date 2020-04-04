6c0327df4d1a3b02ee527803bf76cc2d0c776d62
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-04-05 03:21:15 +0700

    Update customize.sh

--- scripts/openwrt/customize.sh
+++ scripts/openwrt/customize.sh
@@ -5,4 +5,4 @@
 ###########################################################################
 
 # Change default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
