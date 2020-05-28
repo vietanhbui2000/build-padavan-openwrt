45f6ccc78c53bb79e7e0afc3ff5321059f2e24ac
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-05-28 13:23:25 +0700

    Update customize.sh

--- scripts/openwrt/customize.sh
+++ scripts/openwrt/customize.sh
@@ -8,5 +8,5 @@
 # sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Enable Wi-fi by default
-sed -i '/option disabled/d' /etc/config/wireless
-sed -i '/set wireless.radio${devidx}.disabled/d' /lib/wifi/mac80211.sh
+# sed -i '/option disabled/d' /etc/config/wireless
+# sed -i '/set wireless.radio${devidx}.disabled/d' /lib/wifi/mac80211.sh
