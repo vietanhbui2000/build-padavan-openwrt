0593e11e0975f935f95893b710c6b4c5e84752e6
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-02 18:39:30 +0700

    Update customize.sh

--- scripts/padavan/customize.sh
+++ scripts/padavan/customize.sh
@@ -3,16 +3,23 @@
 # Modifications made by: Việt Anh Bùi | https://github.com/vietanhbui2000 #
 ###########################################################################
 
-# Change default settings
+# Install necessary package(s)
+sudo apt-get -y install unzip
+
+# Patch hanwckf's padavan repository
 # sed -i 's/US/CN/g' user/shared/defaults.h
 # sed -i 's/CST-8/GMT0/g' user/shared/defaults.h
 # sed -i 's/ntp1.aliyun.com/pool.ntp.org/g' user/shared/defaults.h
 # sed -i 's/2001:470:0:50::2/time.nist.gov/g' user/shared/defaults.h
 
-# Include Padavan theme pack
-# Install necessary package(s)
-# sudo apt-get install unzip
+# Patch alxdm's padavan repository
+# Download file(s)
+wget https://github.com/vietanhbui2000/build-padavan-openwrt/raw/master/scripts/padavan/user-patched.zip
 
+# Extract file(s)
+unzip -o user-patched.zip -d opt/padavan-ng/trunk
+
+# Include Padavan theme pack
 # Download file(s)
 # wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14.zip
 # wget https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/PadavanThemePack_v2.4.14_Lite.zip
