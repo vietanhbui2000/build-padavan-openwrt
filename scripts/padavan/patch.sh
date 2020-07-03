efddf9ff5d8475c869f8d709e662c06595c69997
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-03 11:50:48 +0700

    Create patch.sh

---
+++ scripts/padavan/patch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###########################################################################
+# Modifications made by: Việt Anh Bùi | https://github.com/vietanhbui2000 #
+###########################################################################
+
+# Patch hanwckf's Padavan repository
+# Download file
+wget https://github.com/vietanhbui2000/build-padavan-openwrt/raw/master/scripts/padavan/hanwckf-patches.zip
+
+# Extract file
+unzip -o hanwckf-patches.zip
+
+# Patch alxdm's Padavan repository
+# Download file
+# wget https://github.com/vietanhbui2000/build-padavan-openwrt/raw/master/scripts/padavan/alxdm-patches.zip
+
+# Extract file
+# unzip -o alxdm-patches.zip
