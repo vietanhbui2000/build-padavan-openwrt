e9f391da5e8cc53239fecc8bda7d6e5b36f8c0cf
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-04 18:26:38 +0700

    Update customize.sh

--- scripts/openwrt/customize.sh
+++ scripts/openwrt/customize.sh
@@ -5,7 +5,7 @@
 ###########################################################################
 
 # Change default IP
-# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Enable WiFi by default
-# sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
