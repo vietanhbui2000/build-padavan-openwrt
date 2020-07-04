ecb585e99c7bdb194bf35e5e0d0313e40b354f50
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-05 01:01:03 +0700

    Update and rename configs/padavan/Mi-WiFi-Mini/board.h to configs/padavan/MiWiFi-Mini/board.h

--- configs/padavan/MiWiFi-Mini/board.h
+++ configs/padavan/MiWiFi-Mini/board.h
@@ -1,8 +1,8 @@
-/* Xiaomi Mi WiFi Mini */
+/* Xiaomi MiWiFi Mini */
 
-#define BOARD_PID		"Mi-WiFi-Mini"
-#define BOARD_NAME		"Mi WiFi Mini"
-#define BOARD_DESC		"Xiaomi Mi WiFi Mini"
+#define BOARD_PID		"MiWiFi-Mini"
+#define BOARD_NAME		"MiWiFi Mini"
+#define BOARD_DESC		"Xiaomi MiWiFi Mini"
 #define BOARD_VENDOR_NAME	"Xiaomi Communications Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifimini"
