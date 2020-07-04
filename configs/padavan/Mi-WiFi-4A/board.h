1553b781ba78b14073f5b2a74797ba9fe48c9ed0
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-04 16:24:44 +0700

    Update and rename configs/padavan/MiWiFi-4A/board.h to configs/padavan/Mi-WiFi-4A/board.h

--- configs/padavan/Mi-WiFi-4A/board.h
+++ configs/padavan/Mi-WiFi-4A/board.h
@@ -1,9 +1,9 @@
 /* Xiaomi Mi WiFi 4A*/
 
-#define BOARD_PID		"MiWiFi-4A"
-#define BOARD_NAME		"MiWiFi-4A"
+#define BOARD_PID		"Mi-WiFi-4A"
+#define BOARD_NAME		"Mi WiFi 4A"
 #define BOARD_DESC		"Xiaomi Mi WiFi 4A"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_NAME	"XIAOMI Electronics,CO.,LTD."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifi4a"
 #define BOARD_BOOT_TIME		25
