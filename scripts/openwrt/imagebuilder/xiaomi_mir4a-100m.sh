c17fb1c97fe09dd5d3ce2dfc7a6bc5e0581bedf6
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-25 02:53:50 +0700

    Update xiaomi_mir4a-100m.sh

--- scripts/openwrt/imagebuilder/xiaomi_mir4a-100m.sh
+++ scripts/openwrt/imagebuilder/xiaomi_mir4a-100m.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=xiaomi_mir4a-100m \
-     PACKAGES="block-mount dnsmasq-full wpad-openssl kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra luci luci-app-upnp luci-app-wol wget -dnsmasq -wpad-basic"
+     PACKAGES="dnsmasq-full wpad-openssl kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra luci luci-app-upnp luci-app-wol wget -dnsmasq -wpad-basic"
