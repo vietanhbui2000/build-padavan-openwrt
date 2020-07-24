30dee99087a31fb265c03c51a09ade9f10bf41d4
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-25 02:39:40 +0700

    Update miwifi-mini.sh

--- scripts/openwrt/imagebuilder/miwifi-mini.sh
+++ scripts/openwrt/imagebuilder/miwifi-mini.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#####################################################################
+# Original author: Việt Anh Bùi | https://github.com/vietanhbui2000 #
+#####################################################################
+
 make image \
      PROFILE=miwifi-mini \
      PACKAGES="block-mount dnsmasq-full wpad-openssl kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra luci luci-app-upnp luci-app-wol htop nano wget -dnsmasq -wpad-basic"
