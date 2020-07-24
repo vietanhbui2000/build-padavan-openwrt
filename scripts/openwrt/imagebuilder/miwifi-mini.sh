dff87f89da76d202c49e0cabde817499a1f73ed9
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-25 02:31:46 +0700

    Create miwifi-mini.sh

---
+++ scripts/openwrt/imagebuilder/miwifi-mini.sh
@@ -0,0 +1,3 @@
+make image \
+     PROFILE=miwifi-mini \
+     PACKAGES="block-mount dnsmasq-full wpad-openssl kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra luci luci-app-upnp luci-app-wol htop nano wget -dnsmasq -wpad-basic"
