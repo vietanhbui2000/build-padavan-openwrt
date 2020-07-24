efd4d0ca4cd72c3d383a6c36e3825bfc628f6dcd
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-07-25 03:27:53 +0700

    Update dl_imagebuilder.sh

--- scripts/openwrt/dl_imagebuilder.sh
+++ scripts/openwrt/dl_imagebuilder.sh
@@ -4,11 +4,12 @@
 ###########################################################################
 
 # File info
-FILE_NAME="openwrt-imagebuilder-19.07-SNAPSHOT-ramips-mt7620.Linux-x86_64.tar.xz"
-FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME"
-# FILE_NAME="openwrt-imagebuilder-19.07-SNAPSHOT-ramips-mt76x8.Linux-x86_64.tar.xz"
-# FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt76x8/$FILE_NAME"
+FILE_NAME="openwrt-imagebuilder-19.07-SNAPSHOT-ramips-mt7620.Linux-x86_64"
+FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+# FILE_NAME="openwrt-imagebuilder-19.07-SNAPSHOT-ramips-mt76x8.Linux-x86_64"
+# FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt76x8/$FILE_NAME.tar.xz"
 
 # Download and extract file
 wget $FILE_URL
-tar -xvf $FILE_NAME
+tar -xvf $FILE_NAME.tar.xz
+mv $FILE_NAME imagebuilder
