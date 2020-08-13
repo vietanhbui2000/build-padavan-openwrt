b2fcb0294fbe6729557d95930658510a0c90322e
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-08-14 02:30:03 +0700

    Create dl_imagebuilder-2.sh

---
+++ scripts/openwrt/dl_imagebuilder-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###########################################################################
+# Modifications made by: Việt Anh Bùi | https://github.com/vietanhbui2000 #
+###########################################################################
+
+# File info
+FILE_NAME="openwrt-imagebuilder-19.07-SNAPSHOT-ramips-mt76x8.Linux-x86_64"
+FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt76x8/$FILE_NAME.tar.xz"
+
+# Download and extract file
+wget $FILE_URL
+tar -xvf $FILE_NAME.tar.xz
+mv $FILE_NAME imagebuilder
