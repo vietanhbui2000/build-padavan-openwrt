b32fc8f3ce9e3d87123142d2f3a81411c3844d00
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-08-14 12:51:28 +0700

    Update patch.sh

--- scripts/padavan/patch.sh
+++ scripts/padavan/patch.sh
@@ -4,8 +4,8 @@
 ###########################################################################
 
 # File info
-FILE_NAME="hanwckf-patches"
-# FILE_NAME="alxdm-patches"
+# FILE_NAME="hanwckf-patches"
+FILE_NAME="alxdm-patches"
 FILE_URL="https://github.com/vietanhbui2000/build-padavan-openwrt/raw/master/scripts/padavan/$FILE_NAME.zip"
 
 # Download and extract file
