66ff75955b4add20470f1afd1270ebad79757596
vietanhbui2000 <vietanhbui2000@outlook.com>
2020-10-03 10:26:11 +0700

    Update files

--- scripts/padavan/dl_toolchain.sh
+++ scripts/padavan/dl_toolchain.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-###########################################################################
-# Modifications made by: Việt Anh Bùi | https://github.com/vietanhbui2000 #
-###########################################################################
+###############################################################
+# Author(s): Việt Anh Bùi | https://github.com/vietanhbui2000 #
+###############################################################
 
 # File info
+DIR_NAME="toolchain-3.4.x"
 FILE_NAME="mipsel-linux-uclibc"
 FILE_URL="https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/master/scripts/padavan/$FILE_NAME.tar.xz"
 
+# Create new directory
+if [ -d $DIR_NAME ]; then
+	echo "$DIR_NAME exists!"
+	exit
+fi
+mkdir -p $DIR_NAME
+
 # Download and extract file
 wget $FILE_URL
-tar -xvf $FILE_NAME.tar.xz
+tar -xvf $FILE_NAME.tar.xz -C $DIR_NAME
