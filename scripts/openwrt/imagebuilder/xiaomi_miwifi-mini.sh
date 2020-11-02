84608349fe3262b6c2a79771a2de3282bb7b66cf
Việt Anh Bùi <vietanhbui2000@outlook.com>
2020-11-02 12:40:43 +0700

    Update workflow files

--- scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
+++ scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=miwifi-mini \
-     PACKAGES="block-mount dnsmasq-full odhcpd wpad-openssl kmod-ipt-conntrack-extra kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat ntfs-3g htop nano curl wget luci luci-ssl-openssl luci-app-uhttpd luci-app-upnp luci-app-wol luci-app-ttyd luci-app-watchcat luci-app-nlbwmon luci-app-https-dns-proxy -dnsmasq -odhcpd-ipv6only -wpad-basic"
+     PACKAGES="block-mount dnsmasq-full odhcpd wpad-openssl kmod-ipt-conntrack-extra kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat ntfs-3g htop nano curl wget luci luci-app-uhttpd luci-app-upnp luci-app-wol luci-app-ttyd luci-app-watchcat luci-app-nlbwmon luci-app-https-dns-proxy -dnsmasq -odhcpd-ipv6only -wpad-basic"
