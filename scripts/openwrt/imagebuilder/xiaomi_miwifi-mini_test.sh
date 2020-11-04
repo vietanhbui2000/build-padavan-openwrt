947e06a109875eca04b8cf95afdbe0b739dc42c8
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-11-04 13:07:45 +0700

    Create xiaomi_miwifi-mini_test.sh

---
+++ scripts/openwrt/imagebuilder/xiaomi_miwifi-mini_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#####################################################################
+# Original author: Việt Anh Bùi | https://github.com/vietanhbui2000 #
+#####################################################################
+
+make image \
+     PROFILE=miwifi-mini \
+     PACKAGES="block-mount dnsmasq-full odhcpd wpad-openssl kmod-ipt-conntrack-extra kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat ntfs-3g htop nano curl wget luci luci-app-uhttpd luci-app-upnp luci-app-wol luci-app-watchcat luci-app-nlbwmon luci-app-https-dns-proxy -dnsmasq -odhcpd-ipv6only -wpad-basic"
