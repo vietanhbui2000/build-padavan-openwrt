2111bf635780c9e61ad30f4d5986924e7a45bb00
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-12-05 14:36:48 +0700

    Update defaults.h

--- configs/padavan/defaults.h
+++ configs/padavan/defaults.h
@@ -46,7 +46,7 @@
 
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
-#define DEF_TIMEZONE		"CST-8"
+#define DEF_TIMEZONE		"admin"
 #define DEF_NTP_SERVER0		"pool.ntp.org"
 #define DEF_NTP_SERVER1		"time.nist.gov"
 #define DEF_HTTPS_CIPH_LIST	"DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
