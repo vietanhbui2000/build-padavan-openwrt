6c17b1b0d433d5265d4ac35dc6866d3973945145
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-02-08 00:02:12 +0700

    Update xiaomi_miwifi-mini.sh

--- scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
+++ scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=miwifi-mini \
-     PACKAGES="block-mount luci"
+     PACKAGES="block-mount dnsmasq-full wpad-openssl htop nano wget kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-ntfs kmod-fs-vfat ntfs-3g luci luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-app-upnp luci-app-wol luci-app-ttyd luci-app-watchcat luci-app-nlbwmon luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-ttyd-en luci-i18n-watchcat-en luci-i18n-nlbwmon-en -dnsmasq -wpad-basic"
