d4119944d03dc1a1b426f76918ea311fd799930a
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-03-08 23:13:53 +0700

    Update dl_sdk.sh

--- scripts/openwrt/dl_sdk.sh
+++ scripts/openwrt/dl_sdk.sh
@@ -4,12 +4,8 @@
 ###############################################################
 
 # File info
-FILE_NAME="openwrt-sdk-18.06-SNAPSHOT-ramips-mt7620_gcc-7.3.0_musl.Linux-x86_64"
-FILE_URL="https://downloads.openwrt.org/releases/18.06-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
-# FILE_NAME="openwrt-sdk-19.07-SNAPSHOT-ramips-mt7620_gcc-7.5.0_musl.Linux-x86_64"
-# FILE_URL="https://downloads.openwrt.org/releases/19.07-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
-# FILE_NAME="openwrt-sdk-ramips-mt7620_gcc-8.4.0_musl.Linux-x86_64"
-# FILE_URL="https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+FILE_NAME="openwrt-sdk-21.02-SNAPSHOT-ramips-mt7620_gcc-8.4.0_musl.Linux-x86_64"
+FILE_URL="https://downloads.openwrt.org/releases/21.02-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
 wget $FILE_URL
