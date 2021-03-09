5ae2ed6fc3cd97f1fa05752e0fa8831ab059f91f
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-03-09 14:11:36 +0700

    Update xiaomi_miwifi-mini.sh

--- scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
+++ scripts/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
@@ -4,5 +4,5 @@
 #####################################################################
 
 make image \
-     PROFILE=miwifi-mini \
-     PACKAGES="block-mount dnsmasq-full wpad-openssl htop nano wget kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-ntfs kmod-fs-vfat ntfs-3g luci luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-app-upnp luci-app-wol luci-app-ttyd luci-app-watchcat luci-app-nlbwmon luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-ttyd-en luci-i18n-watchcat-en luci-i18n-nlbwmon-en -dnsmasq -wpad-basic"
+     PROFILE=xiaomi_miwifi-mini \
+     PACKAGES="block-mount wpad-wolfssl nano htop curl wget kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-ntfs kmod-fs-vfat ntfs-3g luci luci-app-upnp luci-app-wol luci-app-watchcat luci-app-ttyd luci-app-nlbwmon luci-app-wifischedule luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-watchcat-en luci-i18n-ttyd-en luci-i18n-nlbwmon-en luci-i18n-wifischedule-en -wpad-basic-wolfssl"
