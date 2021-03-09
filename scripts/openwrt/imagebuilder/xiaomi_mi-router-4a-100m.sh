df0566c1a568b6533e0dc58297547402e82a574d
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-03-09 15:22:27 +0700

    Update and rename xiaomi_mir4a-100m.sh to xiaomi_mi-router-4a-100m.sh

--- scripts/openwrt/imagebuilder/xiaomi_mi-router-4a-100m.sh
+++ scripts/openwrt/imagebuilder/xiaomi_mi-router-4a-100m.sh
@@ -4,5 +4,5 @@
 #####################################################################
 
 make image \
-     PROFILE=xiaomi_mir4a-100m \
+     PROFILE=xiaomi_mi-router-4a-100m \
      PACKAGES="wpad-wolfssl nano htop curl wget luci luci-app-upnp luci-app-wol luci-app-watchcat luci-app-ttyd luci-app-nlbwmon luci-app-wifischedule luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-watchcat-en luci-i18n-ttyd-en luci-i18n-nlbwmon-en luci-i18n-wifischedule-en -wpad-basic-wolfssl"
