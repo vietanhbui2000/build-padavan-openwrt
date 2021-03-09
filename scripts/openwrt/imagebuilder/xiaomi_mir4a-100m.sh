c0b323bb73058cda636673fe60dd7beab1736ad2
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-03-09 14:14:47 +0700

    Update xiaomi_mir4a-100m.sh

--- scripts/openwrt/imagebuilder/xiaomi_mir4a-100m.sh
+++ scripts/openwrt/imagebuilder/xiaomi_mir4a-100m.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=xiaomi_mir4a-100m \
-     PACKAGES="dnsmasq-full wpad-openssl htop nano wget luci luci-proto-relay luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-app-upnp luci-app-wol luci-app-ttyd luci-app-watchcat luci-app-nlbwmon luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-ttyd-en luci-i18n-watchcat-en luci-i18n-nlbwmon-en -dnsmasq -wpad-basic"
+     PACKAGES="wpad-wolfssl nano htop curl wget luci luci-app-upnp luci-app-wol luci-app-watchcat luci-app-ttyd luci-app-nlbwmon luci-app-wifischedule luci-proto-relay luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-watchcat-en luci-i18n-ttyd-en luci-i18n-nlbwmon-en luci-i18n-wifischedule-en -wpad-basic-wolfssl"
