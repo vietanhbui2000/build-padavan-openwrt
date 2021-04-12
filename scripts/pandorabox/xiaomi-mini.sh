3e3662fce7a73ec71f3f379cd174af2b444686a0
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-04-12 13:18:11 +0700

    Create xiaomi-mini.sh

---
+++ scripts/pandorabox/xiaomi-mini.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#####################################################################
+# Original author: Việt Anh Bùi | https://github.com/vietanhbui2000 #
+#####################################################################
+
+make image \
+     PROFILE=xiaomi-mini \
+     PACKAGES="luci-theme-material"
