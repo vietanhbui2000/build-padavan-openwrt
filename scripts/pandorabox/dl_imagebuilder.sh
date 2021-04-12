6b44cc238286a3184de2e24da637d495cd89a920
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-04-12 15:23:00 +0700

    Update dl_imagebuilder.sh

--- scripts/pandorabox/dl_imagebuilder.sh
+++ scripts/pandorabox/dl_imagebuilder.sh
@@ -5,7 +5,7 @@
 
 # File info
 FILE_NAME="PandoraBox-ImageBuilder-ralink-mt7620.Linux-x86_64-2018-12-31-git-4b6a3d5ca"
-FILE_URL="https://www.dropbox.com/s/sqx3igeklwsw4jl/$FILE_NAME.tar.xz?dl=1"
+FILE_URL="https://dl.dropboxusercontent.com/s/sqx3igeklwsw4jl/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
 wget $FILE_URL
