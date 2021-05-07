f54fce0e34804a13375136a312eb66bd5e5d5c74
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-05-07 15:17:08 +0700

    Update board.h

--- configs/padavan-ng/MiWiFi-4A/board.h
+++ configs/padavan-ng/MiWiFi-4A/board.h
@@ -1,20 +1,21 @@
-/* Xiaomi Mi Nano (R1CL)*/
+/* Xiaomi MiWiFi 4A */
 
-#define BOARD_PID		"MI-4A"
-#define BOARD_NAME		"MI-4A"
-#define BOARD_DESC		"Xiaomi Mi 4A Wireless Router"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"http://www.mi.com/"
-#define BOARD_MODEL_URL		"http://www.mi.com/miwifilite"
+#define BOARD_PID		"MiWiFi-4A"
+#define BOARD_NAME		"MiWiFi-4A"
+#define BOARD_DESC		"Xiaomi MiWiFi 4A"
+#define BOARD_VENDOR_NAME	"XIAOMI Electronics, CO., LTD."
+#define BOARD_VENDOR_URL	"http://www.mi.com"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifi4a"
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	38
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
-#define BOARD_GPIO_LED_WIFI	11
-#define BOARD_GPIO_LED_POWER	44	/* 11: blue, 37: red, 44: yellow */
-#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_POWER	11	/* 11: Blue, 44: Yellow, 37: Red */
 #undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WIFI	44
+#undef  BOARD_GPIO_LED_ROUTER
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
