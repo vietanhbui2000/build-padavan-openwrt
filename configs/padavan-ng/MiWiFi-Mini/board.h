829a720fe91aacab96ae643effe515ae678aa6e9
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-05-07 15:13:56 +0700

    Update board.h

--- configs/padavan-ng/MiWiFi-Mini/board.h
+++ configs/padavan-ng/MiWiFi-Mini/board.h
@@ -1,22 +1,22 @@
-/* Xiaomi Mi Mini */
+/* Xiaomi MiWiFi Mini */
 
-#define BOARD_PID		"MI-MINI"
-#define BOARD_NAME		"MI-MINI"
-#define BOARD_DESC		"Xiaomi Mi Mini Wireless Router"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_PID		"MiWiFi-Mini"
+#define BOARD_NAME		"MiWiFi-Mini"
+#define BOARD_DESC		"Xiaomi MiWiFi Mini"
+#define BOARD_VENDOR_NAME	"Xiaomi Communications Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com"
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifimini"
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	30
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI
-#define BOARD_GPIO_LED_POWER	24	/* 24: blue, 26: yellow, 29: red */
-#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_POWER	24	/* 24: Blue, 26: Yellow, 29: Red */
 #undef  BOARD_GPIO_LED_WAN
-#define BOARD_GPIO_LED_USB	26
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WIFI	26
 #undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_LED_USB	29
 #define BOARD_GPIO_PWR_USB	65
 #define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
 #define BOARD_HAS_5G_11AC	1
