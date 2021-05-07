7c7b9d9da1f03e2cdb8e695b1d3be589adb7b365
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-05-07 19:15:50 +0700

    Create dl_toolchain.sh

---
+++ scripts/padavan-ng/dl_toolchain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###############################################################
+# Author(s): Việt Anh Bùi | https://github.com/vietanhbui2000 #
+###############################################################
+
+# File info
+FILE_NAME="out"
+FILE_URL="https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/main/scripts/padavan-ng/$FILE_NAME.tar.xz"
+
+# Download and extract file(s)
+wget $FILE_URL
+tar -xvf $FILE_NAME.tar.xz
