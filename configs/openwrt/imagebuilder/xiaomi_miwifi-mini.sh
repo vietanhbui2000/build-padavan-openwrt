e78f720cf082c4fc08b80fa5ea3ad907d6987596
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2021-10-30 11:52:37 +0700

    Update xiaomi_miwifi-mini.sh

--- configs/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
+++ configs/openwrt/imagebuilder/xiaomi_miwifi-mini.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=xiaomi_miwifi-mini \
-     PACKAGES="dnsmasq-full wpad-wolfssl block-mount htop nano curl wget-ssl ariang kmod-fs-ext4 kmod-fs-ntfs kmod-fs-exfat kmod-fs-vfat ntfs-3g kmod-usb-storage kmod-usb-storage-extras luci luci-proto-relay luci-app-firewall luci-app-opkg luci-app-ttyd luci-app-ksmbd luci-app-aria2 luci-app-minidlna luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-i18n-ttyd-en luci-i18n-ksmbd-en luci-i18n-aria2-en uci-i18n-minidlna-en -dnsmasq -wpad-basic-wolfssl"
+     PACKAGES="wpad-wolfssl block-mount htop nano curl wget-ssl ariang kmod-fs-ext4 kmod-fs-ntfs kmod-fs-exfat kmod-fs-vfat ntfs-3g kmod-usb-storage kmod-usb-storage-extras luci luci-proto-relay luci-app-firewall luci-app-opkg luci-app-upnp luci-app-wol luci-app-statistics luci-app-ttyd luci-app-watchcat luci-app-ksmbd luci-app-aria2 luci-app-minidlna luci-i18n-base-en luci-i18n-firewall-en luci-i18n-opkg-en luci-i18n-upnp-en luci-i18n-wol-en luci-i18n-statistics-en luci-i18n-ttyd-en luci-i18n-watchcat-en luci-i18n-ksmbd-en luci-i18n-aria2-en uci-i18n-minidlna-en -wpad-basic-wolfssl"
