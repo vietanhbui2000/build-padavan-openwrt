b3d5d94ecd52bbf4886b31eec4c80437736aab76
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-08-29 00:39:02 +0700

    Update dl_toolchain.sh

--- scripts/padavan/dl_toolchain.sh
+++ scripts/padavan/dl_toolchain.sh
@@ -4,9 +4,10 @@
 ###############################################################
 
 # File info
-FILE_NAME="toolchain-3.4.x"
+DIR_NAME="toolchain-3.4.x"
+FILE_NAME="padavan-tlc"
 FILE_URL="https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/main/scripts/padavan/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
-wget $FILE_URL
+curl $FILE_URL
 tar -xvf $FILE_NAME.tar.xz
