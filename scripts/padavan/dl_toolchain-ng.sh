96cb359f40ab920c33d5b51af17f9c392aac91de
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-08-31 02:33:16 +0700

    Update dl_toolchain-ng.sh

--- scripts/padavan/dl_toolchain-ng.sh
+++ scripts/padavan/dl_toolchain-ng.sh
@@ -5,7 +5,7 @@
 
 # File info
 DIR_NAME="out"
-FILE_NAME="padavan-ng-tlc"
+FILE_NAME="padavan-ng-tlc_test"
 FILE_URL="https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/main/scripts/padavan/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
